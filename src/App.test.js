9314d8c7a9a04fc0ee57e3cc5c7c5a5544f91183
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-11-17 01:41:50 -0500

    Fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
+/*
+
+// This is the default test!!
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+*/
+
+test('renders', () => {
+  render(<App />)
 })
